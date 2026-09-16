ed39a83622c08ebbf803312bcafe953dafe47814
William Prayze Pierce <prayze.pierce@gmail.com>
2026-09-15 19:06:42 -0500

    Update cache priorirty

--- service-wroker.js
+++ service-wroker.js
@@ -1,122 +1,83 @@
-const CACHE_NAME =
-    "homework-tracker-v2";
-
+const CACHE_NAME = "homework-tracker-v3";
 
 const FILES_TO_CACHE = [
-
     "./",
-
     "./index.html",
-
     "./style.css",
-
     "./app.js",
-
     "./manifest.json",
-
     "./icons/icon-192.png",
-
     "./icons/icon-512.png"
-
 ];
 
+self.addEventListener("install", event => {
 
-self.addEventListener(
-    "install",
-    event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
 
-        event.waitUntil(
+    self.skipWaiting();
+});
 
-            caches
-                .open(CACHE_NAME)
 
-                .then(
-                    cache => {
+self.addEventListener("activate", event => {
 
-                        return cache.addAll(
-                            FILES_TO_CACHE
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+
+            return Promise.all(
+
+                cacheNames.map(cacheName => {
+
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+
+                })
+
+            );
+
+        })
+    );
+
+    self.clients.claim();
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        fetch(event.request)
+
+            .then(response => {
+
+                const responseClone =
+                    response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+
+                        cache.put(
+                            event.request,
+                            responseClone
                         );
 
-                    }
-                )
+                    });
 
-        );
+                return response;
 
-        self.skipWaiting();
+            })
 
-    }
-);
+            .catch(() => {
 
-
-self.addEventListener(
-    "activate",
-    event => {
-
-        event.waitUntil(
-
-            caches
-                .keys()
-
-                .then(
-                    cacheNames => {
-
-                        return Promise.all(
-
-                            cacheNames.map(
-                                cacheName => {
-
-                                    if (
-                                        cacheName !==
-                                        CACHE_NAME
-                                    ) {
-
-                                        return caches.delete(
-                                            cacheName
-                                        );
-
-                                    }
-
-                                }
-                            )
-
-                        );
-
-                    }
-                )
-
-        );
-
-        self.clients.claim();
-
-    }
-);
-
-
-self.addEventListener(
-    "fetch",
-    event => {
-
-        event.respondWith(
-
-            caches
-                .match(
+                return caches.match(
                     event.request
-                )
+                );
 
-                .then(
-                    response => {
+            })
 
-                        return (
-                            response ||
-                            fetch(
-                                event.request
-                            )
-                        );
+    );
 
-                    }
-                )
-
-        );
-
-    }
-);
+});
